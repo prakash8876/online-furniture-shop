75372b108479d2254dc282c74a117d5a06cc93e3
prakash8876 <prakash8876@gmail.com>
2023-04-14 18:54:11 +0530

    exception added

---
+++ BACKEND/item-service/src/main/java/io/matoshri/item/exception/ResourceGlobalHandler.java
@@ -0,0 +1,36 @@
+package io.matoshri.item.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ResourceGlobalHandler {
+
+
+    @ExceptionHandler({ResourceNotFoundException.class})
+    public ResponseEntity<ApiError> handleResourceNotFoundException(ResourceNotFoundException ex,
+                                                                    HttpServletRequest request) {
+        ApiError error = ApiError.builder()
+                .timestamp(LocalDateTime.now())
+                .path(request.getRequestURI())
+                .error(ex.getMessage())
+                .status(HttpStatus.NOT_FOUND.value())
+                .build();
+        return ResponseEntity.ok(error);
+    }
+
+    @ExceptionHandler({RuntimeException.class, Exception.class})
+    public ResponseEntity<ApiError> handleExceptions(RuntimeException ex,
+                                                                    HttpServletRequest request) {
+        ApiError error = new ApiError();
+        error.setTimestamp(LocalDateTime.now());
+        error.setPath(request.getRequestURI());
+        return ResponseEntity.ok(error);
+    }
+
+}
---
+++ BACKEND/item-service/src/main/java/io/matoshri/item/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package io.matoshri.item.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 4843024917882435644L;
+
+    public ResourceNotFoundException() {
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
